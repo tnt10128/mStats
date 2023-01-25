5c32e75a07cd0adeff18d5f7b5315ee5d5eeefa2
TNT10128 <tntthenoob@gmail.com>
2023-01-25 17:38:51 -0500

    Remove unnecessary parameter from `execute` function in ready.ts

--- src/event/ready.ts
+++ src/event/ready.ts
@@ -5,7 +5,7 @@ import { client, logJoinedGuildCount } from '../index.js';
 export const event = discordJs.Events.ClientReady;
 export const once = true;
 
-export async function execute(guild: discordJs.Guild) {
+export async function execute() {
     if (client.user) {
         console.log(`Successfully logged in as ${client.user.tag}`);
         client.user.setActivity('Minecraft servers', { type: discordJs.ActivityType.Watching });
